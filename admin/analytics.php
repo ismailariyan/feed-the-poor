c26053167cd8203186c051d414c3868fe7164c7b
Elaf24 <alhadi.elaf2428@gmail.com>
2024-06-06 03:37:31 -1200

    ariyan ar deya admin ar analytics commit korsi

--- admin/analytics.php
+++ admin/analytics.php
@@ -173,15 +173,15 @@ if($_SESSION['name']==''){
             $ro2=mysqli_fetch_assoc($res2);
             $female=$ro2['count'];
             $male=$row['count'];
-            $q3="SELECT count(*) as count FROM food_donations where location=\"madurai\"";
+            $q3="SELECT count(*) as count FROM food_donations where location=\"dhaka\"";
             $res3=mysqli_query($connection, $q3);
             $ro3=mysqli_fetch_assoc($res3);
             $madurai=$ro3['count'];
-            $q4="SELECT count(*) as count FROM food_donations where location=\"chennai\"";
+            $q4="SELECT count(*) as count FROM food_donations where location=\"chittagong\"";
             $res4=mysqli_query($connection, $q4);
             $ro4=mysqli_fetch_assoc($res4);
             $chennai=$ro4['count'];
-            $q5="SELECT count(*) as count FROM food_donations where location=\"coimbatore\"";
+            $q5="SELECT count(*) as count FROM food_donations where location=\"sylhet\"";
             $res5=mysqli_query($connection, $q5);
             $ro5=mysqli_fetch_assoc($res5);
             $coimbatore=$ro5['count'];
@@ -189,7 +189,7 @@ if($_SESSION['name']==''){
 
     ?>
 var xValues = ["Male","Female"];
-// var xplace=["Madurai","chennai","coimbatore"];
+var xplace=["dhaka","chittagong","sylhet"];
 var yplace=[<?php echo json_encode($madurai,JSON_HEX_TAG);?>,<?php echo json_encode($coimbatore,JSON_HEX_TAG);?>,<?php echo json_encode($chennai,JSON_HEX_TAG);?>];
 var yValues = [<?php echo json_encode($male,JSON_HEX_TAG);?>,<?php echo json_encode($female,JSON_HEX_TAG);?>,30];
 var barColors = ["#06C167","blue"];
@@ -226,7 +226,7 @@ new Chart("donateChart", {
     legend: {display: false},
     title: {
       display: true,
-      text: "food donation details"
+      text: "Donation details"
     }
   }
 });
